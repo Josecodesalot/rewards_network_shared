3158e5276206b3b729b721b7a7bef4fdf83472b4
Josecodesalot <jose@josedoes.com>
2021-04-28 05:50:57 -0400

    added exports for all the relevant objects

--- lib/reward_network_shared.dart
+++ lib/reward_network_shared.dart
@@ -1,5 +1,9 @@
 export 'models/admin.dart';
 export 'models/clerk.dart';
-export 'models/master.dart';
 export 'models/client.dart';
+export 'models/client_place.dart';
+export 'models/history_ticket.dart';
+export 'models/master.dart';
+export 'models/place_client.dart';
+export 'models/user.dart';
 export 'models/user_type.dart';
