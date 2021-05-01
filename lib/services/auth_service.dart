c2c019f3d47c97b852f4e3b95883674ff59a00c8
Josecodesalot <jose@josedoes.com>
2021-05-01 07:45:00 -0400

    Fixed typing issue for auth service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,11 +2,11 @@ import 'package:rewards_network_shared/models/auth_fields.dart';
 import 'package:rewards_network_shared/models/client_state.dart';
 import 'package:rewards_network_shared/models/response.dart';
 
-abstract class AuthService{
-  Future<Response<UserState>> checkAuthStatus(){}
-  Future<Response<UserState>> signUp(AuthFields authFields){}
-  Future<Response<UserState>> signIn(AuthFields authFields){}
-  Future<Response<UserState>> signOut(){}
-  Future<Response<UserState>> getUserObject(String uid){}
-  Future<Response<UserState>> signInAnon(){}
+abstract class AuthService<User>{
+  Future<Response<User>> checkAuthStatus(){}
+  Future<Response<User>> signUp(AuthFields authFields){}
+  Future<Response<User>> signIn(AuthFields authFields){}
+  Future<Response<User>> signOut(){}
+  Future<Response<User>> getUserObject(String uid){}
+  Future<Response<User>> signInAnon(){}
 }
