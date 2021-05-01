e5f13f6978f431dcd0fcb05f07660f5eecab70f5
Josecodesalot <jose@josedoes.com>
2021-05-01 06:01:25 -0400

    Added theme to be shared between modules

---
+++ lib/const/theme.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// turn into const
+
+TextTheme get baseTheme => GoogleFonts.alataTextTheme();
+TextStyle get baseStyle => GoogleFonts.alata();
+
+Color textColor = Colors.black;
+Color primary = Colors.green[400];
+Color loadingGreen = Colors.green;
+
+BorderRadius get borderRadius => BorderRadius.circular(32);
+const double horizontalPadding = 24.0;
+
+get _headline4 => baseStyle.copyWith(
+  fontSize: 36,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 1,
+  color: textColor.withOpacity(.8),
+);
+
+get _subtitle1  => baseStyle.copyWith(
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+  letterSpacing: .6
+);
+
+TextTheme get _textTheme =>
+    baseTheme.copyWith(
+        headline4: _headline4,
+        subtitle1: _subtitle1,
+    );
+
+ThemeData get lightTheme => ThemeData(
+      primaryColor: primary,
+      textTheme: _textTheme,
+      inputDecorationTheme: InputDecorationTheme(
+        border: UnderlineInputBorder(),
+        labelStyle: baseStyle.copyWith(
+          fontSize: 16,
+        ),
+      ),
+    );
+
+//  InputDecoration inputDecoration(String label) {
+//   return InputDecoration(
+//     labelText: label,
+//     border: OutlineInputBorder(),
+//   );
+// }
