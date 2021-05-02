822168e6284422b5a32e3e5221c4a597c19bc99f
Josecodesalot <jose@josedoes.com>
2021-05-02 11:48:54 -0400

    AuthFields now support placeID for custom signIn behaviour from clerk account

--- lib/models/auth_fields.dart
+++ lib/models/auth_fields.dart
@@ -2,15 +2,20 @@ import 'package:meta/meta.dart';
 
 @immutable
 class AuthFields{
-  const AuthFields({@required this.email, @required this.password, this.name});
-  final String email, password, name;
+  const AuthFields({this.email, this.password, this.name, this.placeId});
+  final String email, password, name, placeId;
 
-
-  AuthFields copyWith({String email, String password, String name}){
+  AuthFields copyWith({
+    String email,
+    String password,
+    String name,
+    String placeId,
+  }) {
     return AuthFields(
-      email: email??this.email,
-      password: password??this.password,
-      name:  name??this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      name: name ?? this.name,
+      placeId: placeId ?? this.placeId,
     );
   }
 }
